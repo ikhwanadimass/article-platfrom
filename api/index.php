73f25b1f1ec04226d1f716a5162989e13e07b0a9
ikhwanadimass <saudaraikhwanadimas6@gmail.com>
2026-09-24 00:24:15 +0700

    fix

--- api/index.php
+++ api/index.php
@@ -1,4 +1,18 @@
 <?php
 
+// Pastikan folder cache dan view Laravel di /tmp tersedia
+$dirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+];
+
+foreach ($dirs as $dir) {
+    if (! is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 // Forward request Vercel Serverless ke Laravel public/index.php
-require __DIR__ . '/../public/index.php';
+require __DIR__.'/../public/index.php';
